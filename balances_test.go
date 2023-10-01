171af296d770e574d4002d52082639af1599457b
Mohamed Yousif <mmbusif@gmail.com>
2023-10-01 14:29:04 +0400

    update ledger

--- balances_test.go
+++ balances_test.go
@@ -9,16 +9,21 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-func Test_transferCredits(t *testing.T) {
-	var sess *session.Session
-	sess, err = session.NewSession(&aws.Config{
+var _testSess *session.Session
+var _dbSvc *dynamodb.DynamoDB
+
+func init() {
+	_testSess, err = session.NewSession(&aws.Config{
 		Region: aws.String("eu-north-1"),
 	})
 	if err != nil {
 		log.Fatal("Failed to create DynamoDB session:", err)
 	}
+	_dbSvc = dynamodb.New(_testSess)
+}
 
-	dyn := dynamodb.New(sess)
+func Test_transferCredits(t *testing.T) {
+
 	type args struct {
 		dbSvc         *dynamodb.DynamoDB
 		fromAccountID string
@@ -30,7 +35,7 @@ func Test_transferCredits(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		{"testing transfer", args{fromAccountID: "adonese", toAccountID: "mj", dbSvc: dyn, amount: 10}, false},
+		{"testing transfer", args{fromAccountID: "adonese", toAccountID: "mj", dbSvc: _dbSvc, amount: 10}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -42,15 +47,6 @@ func Test_transferCredits(t *testing.T) {
 }
 
 func Test_inquireBalance(t *testing.T) {
-	var sess *session.Session
-	sess, err = session.NewSession(&aws.Config{
-		Region: aws.String("eu-north-1"),
-	})
-	if err != nil {
-		log.Fatal("Failed to create DynamoDB session:", err)
-	}
-
-	dyn := dynamodb.New(sess)
 	type args struct {
 		dbSvc     *dynamodb.DynamoDB
 		AccountID string
@@ -61,7 +57,7 @@ func Test_inquireBalance(t *testing.T) {
 		want    float64
 		wantErr bool
 	}{
-		{"test-get-balance", args{dbSvc: dyn, AccountID: "adonese"}, 30, false},
+		{"test-get-balance", args{dbSvc: _dbSvc, AccountID: "249_ACCT_1"}, 30, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -72,6 +68,27 @@ func Test_inquireBalance(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("inquireBalance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_createAccountWithBalance(t *testing.T) {
+	type args struct {
+		dbSvc     *dynamodb.DynamoDB
+		accountId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"generate account with balance", args{dbSvc: _dbSvc, accountId: "249_ACCT_1"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := createAccountWithBalance(tt.args.dbSvc, tt.args.accountId); (err != nil) != tt.wantErr {
+				t.Errorf("createAccountWithBalance() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
