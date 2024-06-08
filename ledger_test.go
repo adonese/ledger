5c8ef0205485d4ce4b171cd94e063b65b02648e2
Mohamed Yousif <mmbusif@gmail.com>
2024-06-08 12:47:07 +0300

    Add delete account api

--- ledger_test.go
+++ ledger_test.go
@@ -1,7 +1,10 @@
 package ledger
 
 import (
+	"context"
 	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 var _AWS_REGION = "us-east-1"
@@ -28,6 +31,29 @@ func TestInitializeLedger(t *testing.T) {
 			if got == nil {
 				t.Errorf("InitializeLedger() error = ledger is nil")
 				return
+			}
+		})
+	}
+}
+
+func TestDeleteAccount(t *testing.T) {
+	type args struct {
+		ctx       context.Context
+		dbSvc     *dynamodb.Client
+		tenantId  string
+		accountId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"test_delete_account", args{ctx: context.TODO(), dbSvc: _dbSvc, tenantId: "nil", accountId: "+1234567890"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := DeleteAccount(tt.args.ctx, tt.args.dbSvc, tt.args.tenantId, tt.args.accountId); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteAccount() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
